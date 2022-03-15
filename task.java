e0a3c14b3fcdb92dbdc5730fbb689ba03746332e
pratibha398 <101618155+pratibha398@users.noreply.github.com>
2022-03-15 12:16:05 +0530

    Add files via upload

---
+++ task.java
@@ -0,0 +1,9 @@
+class task
+{
+	public static void main(String args[])
+	{
+		System.out.println("Name: Pratibha Dewangan");
+		System.out.println("Batch: 'B'");
+		System.out.println("Course: CDAC");
+	}
+}
